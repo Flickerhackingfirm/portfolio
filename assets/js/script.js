7fbb6f1a402c77eaf81263487f8302915ee0fb41
Flickerhackingfirm <flickerhackingfirm@gmail.com>
2025-07-17 06:26:36 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -218,15 +218,15 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-// (function () {
-//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-//     s1.async = true;
-//     s1.src = 'https://embed.tawk.to/68757d7237f49b190d0f97a7/1j05f42bq';
-//     s1.charset = 'UTF-8';
-//     s1.setAttribute('crossorigin', '*');
-//     s0.parentNode.insertBefore(s1, s0);
-// })();
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/68757d7237f49b190d0f97a7/1j05f42bq';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
 // End of Tawk.to Live Chat
 
 
